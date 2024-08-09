1f933139cd633e526ae4ef5c33a6186001ea3e8b
taogaetz <59668529+taogaetz@users.noreply.github.com>
2024-08-09 14:54:57 -0400

    Init commit

---
+++ ui/nuxt.config.ts
@@ -0,0 +1,11 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+    vite: {
+      build: { target: "esnext" },
+      optimizeDeps: { esbuildOptions: { target: "esnext" } },
+    },
+    css: ['~/assets/styles/globals.css'],
+  
+  compatibilityDate: '2024-04-03',
+  devtools: { enabled: true }
+})
