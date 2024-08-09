75bef6f6d0e7ed5c8400503cbc51f92e22dea183
taogaetz <59668529+taogaetz@users.noreply.github.com>
2024-08-09 17:25:23 -0400

    completed tutorial

---
+++ contracts/src/main.ts
@@ -0,0 +1,54 @@
+import { Square } from './Square.js';
+import { Field, Mina, PrivateKey, AccountUpdate } from 'o1js';
+
+const useProof = false;
+
+const Local = await Mina.LocalBlockchain({ proofsEnabled: useProof });
+Mina.setActiveInstance(Local);
+
+const deployerAccount = Local.testAccounts[0];
+const deployerKey = deployerAccount.key;
+const senderAccount = Local.testAccounts[1];
+const senderKey = senderAccount.key;
+
+
+
+// swag town
+
+const zkAppPrivateKey = PrivateKey.random();
+const zkAppAddress = zkAppPrivateKey.toPublicKey();
+
+
+//  new square instance:
+
+const zkAppInstance = new Square(zkAppAddress);
+const deployTxn = await Mina.transaction(deployerAccount, async () => {
+  AccountUpdate.fundNewAccount(deployerAccount);
+  await zkAppInstance.deploy();
+});
+await deployTxn.sign([deployerKey, zkAppPrivateKey]).send();
+
+// get init state post-deploy
+const num0 = zkAppInstance.num.get();
+console.log('state after init:', num0.toString());
+
+const txn1 = await Mina.transaction(senderAccount, async () => {
+  await zkAppInstance.update(Field(9));
+});
+await txn1.prove();
+await txn1.sign([senderKey]).send();
+
+const num1 = zkAppInstance.num.get();
+console.log('state after txn1')
+
+try {
+  const txn2 = await Mina.transaction(senderAccount, async () => {
+    await zkAppInstance.update(Field(81));
+  });
+  await txn2.prove();
+  await txn2.sign([senderKey]).send();
+} catch(error: any) {
+  console.log(error.message);
+}
+const num2 = zkAppInstance.num.get();
+console.log('state after txn2:', num2.toString());
